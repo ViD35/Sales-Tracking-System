a9d1a37ecaa07dd80ed5f3310bb8eec13d914131
ViD35 <vaeh2000@hotmail.es>
2023-12-03 14:24:01 -0600

    UserForm test draft

--- app/src/main/java/com/vid35/controldeventas/MainActivity.kt
+++ app/src/main/java/com/vid35/controldeventas/MainActivity.kt
@@ -3,13 +3,26 @@ package com.vid35.controldeventas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.vid35.controldeventas.ui.theme.ControlDeVentasTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TestForm("ViD35")
                 }
             }
         }
@@ -30,17 +43,82 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun TestForm(name: String, modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
     )
+    {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Text(
+            text = "I hope you don't mind I write some random text to try things out...",
+            modifier = modifier
+        )
+    }
+}
+
+@Composable
+fun TestUserForm(name: String, modifier: Modifier = Modifier) {
+    val subModifier = Modifier.padding(6.dp)
+    var nombre by remember {
+        mutableStateOf("")
+    }
+    var apellido by remember {
+        mutableStateOf("")
+    }
+    var telefono by remember {
+        mutableStateOf("")
+    }
+    Column(
+        horizontalAlignment = Alignment.Start,
+        modifier = modifier
+    ) {
+        Text(text = "Nombre", subModifier)
+        OutlinedTextField(
+            value = nombre,
+            onValueChange = { text ->
+                nombre = text
+            }, subModifier
+        )
+        Text(text = "Apellido", subModifier)
+        OutlinedTextField(
+            value = apellido,
+            onValueChange = { text ->
+                apellido = text
+            }, subModifier
+        )
+        Text(text = "Teléfono", subModifier)
+        OutlinedTextField(
+            value = telefono,
+            onValueChange = { text ->
+                telefono = text
+            }, subModifier
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text(text = "Ingresar", subModifier)
+            Button(onClick = {
+
+            }, subModifier) {
+                Text(text = "Submit")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ControlDeVentasTheme {
-        Greeting("Android")
+        TestUserForm("Maria",
+            Modifier
+            .fillMaxSize()
+            .padding(12.dp)
+        )
     }
 }
